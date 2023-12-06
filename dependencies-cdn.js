bdb84c25c9f8a0853a2723a5881c3899653ba917
zhangguo <zhangguo@ulearning.cn>
2023-12-06 20:17:01 +0800

    备个份

---
+++ dependencies-cdn.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: 姚文彬
+ * @Date: 2021-06-03 17:12:20
+ * @LastEditors: 姚文彬
+ * @LastEditTime: 2021-06-03 21:01:23
+ * @FilePath: \p-front-admin\dependencies-cdn.js
+ */
+const timestamp = new Date().getTime();
+
+module.exports = [
+  {
+    name: 'vue',
+    library: 'Vue',
+    js: 'https://static.peixunyun.cn/plugins/vue%402.7.13/vue.min.js',
+    css: ''
+  },
+]
